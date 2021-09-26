256c821dda7013ba79bda9bea57a2752c7fe28a7
moka <moka.drip.web@gmail.com>
2021-09-26 13:31:16 +0900

    完成した！

--- pyTodo/app.py
+++ pyTodo/app.py
@@ -2,7 +2,7 @@
 # pyTodo.app.py
 
 import sys
-
+import pandas as pd
 
 def run():
     try:
@@ -10,23 +10,34 @@ def run():
 
         if args[1] == "add":
             print("Add: " + args[2])
-            f = open('todo.txt', mode='a')
+            f = open("todo.csv", "a")
             f.write(args[2] + "\n")
             f.close()
             print("Completed 👍")
 
         elif args[1] == "show":
-            with open('todo.txt', 'r') as f:
-                keyword_list = f.read().split("\n")
-                print(keyword_list)
-                f.close()
+            df = pd.read_csv("todo.csv")
+            print(df)
+
+        elif args[1] == "clear":
+            with open("todo.csv", 'w') as f:
+                pass
+            print("success!")
+
+        elif args[1] == "delete":
+            listNum = args[2]
+            df = pd.read_csv("todo.csv")
+            df = df.drop(int(listNum), axis=0)
+            print(df)
+            df.to_csv('todo.csv', index=False)
+
         else:
             print("?: The value is different from the defined value.")
+
 
     except:
         print("Error:The value you entered is different from the defined value.\n")
         print("-" * 30)
         print("Add: todo add [value]")
         print("Show: todo show")
-        print("Delete: todo delete [listNum]")
         print("-" * 30)
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="todo",
     version="1.0.0",
+    install_requires=["pandas"],
     entry_points={
         "console_scripts": [
             "todo = main:main"
